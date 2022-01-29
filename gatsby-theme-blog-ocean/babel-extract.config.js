6225a91aeebb247aabf7a0dd48c8db67fb84f5d7
Howyoung <howyoung3@gmail.com>
2022-01-29 21:56:19 +0800

    Add extract command

---
+++ gatsby-theme-blog-ocean/babel-extract.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: [
+    [
+      'i18next-extract',
+      {
+        locales: ['en'],
+        keySeparator: null,
+        nsSeparator: null,
+        keyAsDefaultValue: ['en'],
+        useI18nextDefaultValue: ['en'],
+        discardOldKeys: true,
+        outputPath: 'src/locales/{{locale}}/{{ns}}.json',
+        customTransComponents: [['gatsby-plugin-react-i18next', 'Trans']]
+      }
+    ]
+  ],
+  overrides: [
+    {
+      test: [`**/*.ts`, `**/*.tsx`],
+      plugins: [[`@babel/plugin-transform-typescript`, { isTSX: true }]]
+    }
+  ]
+};
