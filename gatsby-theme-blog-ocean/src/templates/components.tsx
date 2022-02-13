d7c48d68d6de5dae11913f78a0f08341b1789a7c
Howyoung <howyoung3@gmail.com>
2022-02-13 18:03:18 +0800

    Adjust element spacing of post

--- gatsby-theme-blog-ocean/src/templates/components.tsx
+++ gatsby-theme-blog-ocean/src/templates/components.tsx
@@ -16,27 +16,27 @@ import CodeBlock from '../components/code-block';
 
 const components = {
   p: (() => {
-    const Paragraph = props => <Typography {...props} style={{ marginTop: 0, marginBottom: 16 }} />;
+    const Paragraph = props => <Typography {...props} style={{ margin: '8px 0' }} />;
     return memo(Paragraph);
   })(),
   h1: (() => {
-    const H1 = props => <Typography {...props} component="h1" variant="h3" style={{ margin: '16px 0' }} />;
+    const H1 = props => <Typography {...props} component="h1" variant="h3" style={{ margin: '24px 0 8px' }} />;
     return memo(H1);
   })(),
   h2: (() => {
-    const H2 = props => <Typography {...props} component="h2" variant="h4" style={{ margin: '40px 0 16px' }} />;
+    const H2 = props => <Typography {...props} component="h2" variant="h4" style={{ margin: '24px 0 8px' }} />;
     return memo(H2);
   })(),
   h3: (() => {
-    const H3 = props => <Typography {...props} component="h3" variant="h5" style={{ margin: '40px 0 16px' }} />;
+    const H3 = props => <Typography {...props} component="h3" variant="h5" style={{ margin: '24px 0 8px' }} />;
     return memo(H3);
   })(),
   h4: (() => {
-    const H4 = props => <Typography {...props} component="h4" variant="h6" style={{ margin: '32px 0 16px' }} />;
+    const H4 = props => <Typography {...props} component="h4" variant="h6" style={{ margin: '16px 0 8px' }} />;
     return memo(H4);
   })(),
   h5: (() => {
-    const H5 = props => <Typography {...props} component="h5" variant="subtitle2" style={{ margin: '32px 0 16px' }} />;
+    const H5 = props => <Typography {...props} component="h5" variant="subtitle2" style={{ margin: '16px 0 8px' }} />;
     return memo(H5);
   })(),
   h6: (() => {
@@ -47,7 +47,7 @@ const components = {
     const Blockquote = props => {
       const theme = useTheme();
       return (
-        <Paper style={{ borderLeft: `5px solid ${theme.palette.primary.light}`, padding: '4px 24px', margin: '24px 0' }} {...props} />
+        <Paper style={{ borderLeft: `5px solid ${theme.palette.primary.light}`, padding: '4px 24px', margin: '16px 0' }} {...props} />
       )
     };
     return memo(Blockquote);
