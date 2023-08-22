2709d2b14aeb7a274b9e5b7fcfa3cc9123adb25b
Howyoung <howyoung3@gmail.com>
2023-08-22 15:29:30 +0200

    chore: remove unused

--- example/gatsby-config.ts
+++ example/gatsby-config.ts
@@ -1,8 +1,3 @@
-import { dirname } from "path"
-import { fileURLToPath } from "url"
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 export default {
   siteMetadata: {
     author: 'Howyoung',
